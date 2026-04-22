4f5449152e8f8e6a797a2c6022b0cce2f2779098
Brian Henning <henninb@msn.com>
2026-04-22 06:36:01 -0500

    wip

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo npx create-next-app example-nextjs
-echo 'http://localhost:3000/hello'
-echo 'http://localhost:3000/cars'
+echo 'http://localhost:4000/hello'
+echo 'http://localhost:4000/cars'
 #npm install
 touch .env.local
 npm install
